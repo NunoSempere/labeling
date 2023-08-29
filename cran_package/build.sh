7f959bccf6affab8c983c9b5a6a187ae999bcca5
NunoSempere <nuno.sempere@protonmail.com>
2023-08-29 23:02:32 +0200

    add rchecks, build manual from xelatex.

--- cran_package/build.sh
+++ cran_package/build.sh
@@ -2,4 +2,8 @@
 R CMD build labeling
 R CMD check labeling_0.4.3.tar.gz
 echo "There might be problems with pdf generation. These are expected."
-cp ./labeling/man/Rd.sty
+
+cp ./labeling/man/Rd.sty ./labeling.Rcheck
+cd labeling.Rcheck
+xelatex labeling-manual.tex
+cp labeling-manual.pdf ../
