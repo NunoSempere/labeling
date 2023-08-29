603ca2521f1f388706e80646d995a6151104d996
NunoSempere <nuno.sempere@protonmail.com>
2023-08-30 00:06:05 +0200

    update stats scripts.

--- stats/labeling-stats.R
+++ stats/labeling-stats.R
@@ -1,13 +1,37 @@
 # install.packages("dlstats")
 library(dlstats)
 library(ggplot2)
+library(ggthemes)
 
+cat("Fetching stats...\n")
 x <- cran_stats(c("labeling"))
+cat("Stats fetched.\n")
 
+cat("Plotting stats\n")
 if (!is.null(x)) {
   head(x)
-  ggplot(x, aes(end, downloads, group=package, color=package)) +
-    geom_line() + geom_point(aes(shape=package))
+  title = "Downloads through time for package labeling"
+  ggplot(x, aes(end, downloads)) +
+    geom_line() + 
+    geom_point(aes(shape=package)) + 
+    labs(
+      title="Downloads for R package 'labeling' through time",
+      subtitle="",
+      x="", 
+      y="" 
+    ) +
+    theme_tufte() + 
+    theme(
+      legend.title = element_blank(),
+      plot.title = element_text(hjust = 0.5),
+      plot.subtitle = element_text(hjust = 0.5), 
+      legend.position="none",
+      axis.text.x=element_text(angle=60, hjust=1),
+      plot.background=element_rect(fill = "white",colour = NA)
+    ) + 
+    # scale_x_date(date_breaks = "1 year", date_labels =  "%Y") +
+    scale_y_continuous(labels = scales::label_comma())
 }
 
-ggsave("test.png", width=6, height=4)
+cat("Saving plot\n")
+ggsave("stats.png", width=6, height=4)
