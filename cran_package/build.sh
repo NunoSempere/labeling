13c046ac6b482dd415069761c907a08aeba71d8d
NunoSempere <nuno.sempere@protonmail.com>
2023-08-29 23:43:37 +0200

    build & check one last time

--- cran_package/build.sh
+++ cran_package/build.sh
@@ -3,7 +3,11 @@ R CMD build labeling
 R CMD check labeling_0.4.3.tar.gz
 echo "There might be problems with pdf generation. These are expected."
 
+echo "Building labeling.pdf with xelatex instead"
 cp ./labeling/man/Rd.sty ./labeling.Rcheck
 cd labeling.Rcheck
 xelatex labeling-manual.tex
 cp labeling-manual.pdf ../
+
+R CMD check --as-cran labeling
+
